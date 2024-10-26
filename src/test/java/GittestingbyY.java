2a463a62c6284360e5e566f61d07ad89d8cadfc5
atul <atulpathakdec@gmail.com>
2024-10-26 15:43:03 +0530

    CST timing commited

--- src/test/java/GittestingbyY.java
+++ src/test/java/GittestingbyY.java
@@ -1,11 +1,18 @@
 
 public class GittestingbyY {
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		  System.out.println("post jira 1");
 		  System.out.println("post jira 2");
 		  System.out.println("post jira 3");
+		  
+		  System.out.println("web automation ");
+		  System.out.println("browser automation ");
+		  
+		  
 	}
 
 }
