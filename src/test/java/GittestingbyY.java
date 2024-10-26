e5dec7a9ee38ae7ae48e2011ce07ef4182c9c3f0
atul <atulpathakdec@gmail.com>
2024-10-26 13:56:14 +0530

    this is commited from ISt

---
+++ src/test/java/GittestingbyY.java
@@ -0,0 +1,11 @@
+
+public class GittestingbyY {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  System.out.println("post jira 1");
+		  System.out.println("post jira 2");
+		  System.out.println("post jira 3");
+	}
+
+}
